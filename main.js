3cc48871233144a93fd3805f057655d5a5bb5603
HyunSangHan <gustkd3@gmail.com>
2019-09-03 09:48:07 +0900

    App - Not found 오류 구현, 홈 구현

--- main.js
+++ main.js
@@ -4,39 +4,41 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-        _url = '/index.html';
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="index.html">WEB</a></h1>
-        <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JAVASCRIPT">JAVASCRIPT</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}
-        </p>
-        </body>
-        </html>
-    `;
-    response.end(template);
-    })
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/' || pathname === '/index.html') {
+        var title = queryData.id;
+        fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
+            if(title === undefined){
+                title = "Welcome";
+                description = "Welcome to my homepage"
+            }
 
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="index.html">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JAVASCRIPT">JAVASCRIPT</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}
+            </p>
+            </body>
+            </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+        })
+    } else {
+        response.writeHead(404);
+        response.end("not found");
+    }
 });
 app.listen(3000);
