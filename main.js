eb05e658ae41e4be3dc606112791302d627448e5
HyunSangHan <gustkd3@gmail.com>
2019-09-04 09:00:00 +0900

    App - 함수를 이용해서 정리 정돈하기

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+
+function templateHTML(title, list, body) {
+    return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="index.html">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `
+}
+
+function templateList(filelist) {
+    var list = `<ul>`
+    for(var i = 0; i < filelist.length; i++) {
+        list += `<li><a href="/?id=`+filelist[i]+`">`+filelist[i]+`</a></li>`
+    }
+    return list + `</ul>`
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
+
     if(pathname === '/' || pathname === '/index.html') {
         var title = queryData.id;
         var description = "";
@@ -12,56 +39,17 @@ var app = http.createServer(function(request,response){
             fs.readdir('./data', (err, filelist) => {
                 title = "Welcome";
                 description = "Welcome to my homepage"
-                list = `<ul>`
-                for(var i = 0; i < filelist.length; i++) {
-                    list += `<li><a href="/?id=`+filelist[i]+`">`+filelist[i]+`</a></li>`
-                }
-                list += `</ul>`
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="index.html">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}
-                </p>
-                </body>
-                </html>
-                `;
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
                 response.writeHead(200);
                 response.end(template);    
             });
         } else {
             fs.readdir('./data', (err, filelist) => {
                 fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
-                    console.log(description);
                     description = description;
-                    list = `<ul>`
-                    for(var i = 0; i < filelist.length; i++) {
-                        list += `<li><a href="/?id=`+filelist[i]+`">`+filelist[i]+`</a></li>`
-                    }
-                    list += `</ul>`
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="index.html">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}
-                    </p>
-                    </body>
-                    </html>
-                    `;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
                     response.writeHead(200);
                     response.end(template);
                 })
