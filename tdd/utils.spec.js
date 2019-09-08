748cf8abcf010e83c3e68afbbd0b711981dac719
HyunSangHan <gustkd3@gmail.com>
2019-09-09 01:18:20 +0900

    테스트수트 작성

---
+++ tdd/utils.spec.js
@@ -0,0 +1,7 @@
+const utils = require('./utils') // .js 확장자 생략가능
+
+describe('utils.js 모듈의 capitalize 함수는 ', () => {
+    it('문자열의 첫번째 문자를 대문자로 변환한다.', () => {
+        //test code 들어갈 곳
+    })
+})
