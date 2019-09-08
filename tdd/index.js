574e92b138a16beeb977b696aa51e7fd8fa0d17b
HyunSangHan <gustkd3@gmail.com>
2019-09-09 01:06:52 +0900

    Arrange directories, make get_user API

---
+++ tdd/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var morgan = require('morgan');var users = [
+    {id: 1, name: 'marco'},
+    {id: 2, name: 'anna'},
+    {id: 3, name: 'egoing'},
+];
+
+app.use(morgan('dev'));
+
+app.get('/users', function(req, res) {
+    res.json(users);
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
