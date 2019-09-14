2b5220e015d3c9954a347084b6ef44a6ca05768d
HyunSangHan <gustkd3@gmail.com>
2019-09-14 08:16:36 +0100

    API 테스트 코드 만들기 1

--- tdd/index.spec.js
+++ tdd/index.spec.js
@@ -1,13 +1,16 @@
-const app = require('./index');
 const request = require('supertest');
+const should = require('should');
+const app = require('./index');
 
 describe('GET /users는', () => {
-    it('블라블라', (done) => { //done은 콜백함수임
-        request(app)
-            .get('/users')
-            .end((err,res) => {
-                console.log(res.body);
-                done();
-            })
+    describe('성공 시', () => {
+        it('유저정보를 담은 객체를 반환한다', (done) => { //done은 콜백함수임
+            request(app)
+                .get('/users')
+                .end((err,res) => {
+                    res.body.should.be.instanceOf(Array);
+                    done();
+                })
+        })    
     })
 })
