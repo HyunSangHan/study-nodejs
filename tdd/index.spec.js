c825bb56355089c2069a71b6850b99d20a7621da
HyunSangHan <gustkd3@gmail.com>
2019-09-23 01:31:14 +0100

    개별 사용자 삭제 API

--- tdd/index.spec.js
+++ tdd/index.spec.js
@@ -65,3 +65,22 @@ describe('GET /users 1는', () => {
         })
     })
 });
+
+describe('DELETE /users 1는', () => {
+    describe('성공시', () => {
+        it('204를 응답한다', (done) => {
+            request(app)
+                .delete('/users/1')
+                .expect(204)
+                .end(done);
+        })
+    })
+    describe('실패시', () => {
+        it('id가 숫자형이 아닐 경우 400(bad request)로 응답한다', done => {
+            request(app)
+                .delete('/users/one')
+                .expect(400)
+                .end(done);
+        })
+    })
+});
