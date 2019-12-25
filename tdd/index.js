fe7ddbdb748fc4867d1ff3772d64002ffb8c690d
HyunSangHan <gustkd3@gmail.com>
2019-12-26 01:20:54 +0900

    테스트 환경 개선 1. 서버 구동 중복 로직 제거 2. test 시, 불필요한 로그가 남지 않도록 수정

--- tdd/index.js
+++ tdd/index.js
@@ -4,13 +4,11 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var user = require('./api/user'); // ./api/user/index.js 라고까지 굳이 써줄 필요는 없다.
 
-app.use(morgan('dev')); //middleware를 사용할 때에는 use를 쓴다.
+if (process.env.NODE_ENV !== "test") {
+    app.use(morgan('dev')); //middleware를 사용할 때에는 use를 쓴다.
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', user) // /users에 대한 라우팅은 user라는 모듈이 담당한다는 의미.
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-});
 
 module.exports = app;
