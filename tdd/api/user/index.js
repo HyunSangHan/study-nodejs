26e4acf64192be2e555d3fc316f8aa4a97e7c679
HyunSangHan <gustkd3@gmail.com>
2019-12-26 00:51:13 +0900

    코드 리팩토링1 - 라우터 클래스 이용

--- tdd/api/user/index.js
+++ tdd/api/user/index.js
@@ -1,10 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
 var users = [
     {id: 1, name: 'marco'},
     {id: 2, name: 'anna'},
     {id: 3, name: 'kim'},
 ];
 
-app.get('/users', function(req, res) {
+router.get('/', function(req, res) {
     req.query.limit = req.query.limit || 10;
     const limit = parseInt(req.query.limit, 10);
     if (Number.isNaN(limit)) {
@@ -13,7 +16,7 @@ app.get('/users', function(req, res) {
     res.json(users.slice(0, limit));
 });
 
-app.get('/users/:id', function(req, res) {
+router.get('/:id', function(req, res) {
     const id = parseInt(req.params.id, 10)
     if (Number.isNaN(id)) return res.status(400).end();
     const user = users.filter(user => user.id === id)[0];
@@ -21,14 +24,14 @@ app.get('/users/:id', function(req, res) {
     res.json(user);
 });
 
-app.delete('/users/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id, 10)
     if (Number.isNaN(id)) return res.status(400).end();
     users = users.filter(user => user.id !== id)[0];
     res.status(204).end();
 })
 
-app.post('/users', (req, res) => {
+router.post('/', (req, res) => {
     //원래 express는 body를 지원하지 않으므로 bodyParser를 미들웨어로 추가해줘야 쓸 수 있게 된다.
     const name = req.body.name;
     if (!name) return res.status(400).end();
@@ -39,7 +42,7 @@ app.post('/users', (req, res) => {
     res.status(201).json(user);
 })
 
-app.put('/users/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     const id = parseInt(req.params.id, 10)
     const name = req.body.name;
     if (!name || Number.isNaN(id)) return res.status(400).end();
@@ -49,3 +52,5 @@ app.put('/users/:id', (req, res) => {
     user.name = name;
     res.status(203).json(user);
 })
+
+module.exports = router;
--- tdd/index.js
+++ tdd/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var user = require('./api/user'); // ./api/user/index.js 라고까지 굳이 써줄 필요는 없다.
 
 app.use(morgan('dev')); //middleware를 사용할 때에는 use를 쓴다.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', user) // /users에 대한 라우팅은 user라는 모듈이 담당한다는 의미.
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
