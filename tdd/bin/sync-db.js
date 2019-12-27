98957667020197f05433f10454293b71dc7d7edd
HyunSangHan <gustkd3@gmail.com>
2019-12-27 12:37:14 +0900

    Change force option value by NODE_ENV

--- tdd/bin/sync-db.js
+++ tdd/bin/sync-db.js
@@ -1,7 +1,10 @@
 const models = require("../models");
 
 module.exports = () => {
-    return models.sequelize.sync({force: true})
+    const options = {
+        force: process.env.NODE_ENV === 'test' ? true : false
+    };
+    return models.sequelize.sync(options);
     // force: true 는, 기존 데이터를 모두 날리고 새로 싱크를 맞추겠다는 의미고
     // 참고로, 이렇듯 모델.시퀄라이즈.싱크는 내부적으로 프로미스를 리턴하기 때문에 비동기를 완료할 수 있도록 인터페이스를 지원한다.
 }
